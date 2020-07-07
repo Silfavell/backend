3d1ba3242b7a256ab395b9823f8eca4e38d9d8f1
ipekmuhammet <muhammetipek57@hotmail.com>
2020-07-07 19:31:29 +0300

    coupon code schema init

---
+++ src/models/CouponCode.ts
@@ -0,0 +1,26 @@
+// eslint-disable-next-line no-unused-vars
+import mongoose, { Document, Schema } from 'mongoose'
+
+export type CouponCodeDocument = Document & {
+    couponCode: number,
+    isUsed: boolean,
+    belongTo: mongoose.Types.ObjectId
+}
+
+const couponCodeSchema = new Schema({
+    couponCode: {
+        type: Number,
+        required: true
+    },
+    isUsed: {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+    belongTo: {
+        type: mongoose.Types.ObjectId,
+        required: true
+    }
+})
+
+export default mongoose.model<CouponCodeDocument>('CouponCode', couponCodeSchema)
