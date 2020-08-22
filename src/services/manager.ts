1af7975f6fcb57bff2a20d00bf62d6fd519c32c0
ipekmuhammet <muhammetipek57@hotmail.com>
2020-08-22 03:13:28 +0300

    .

--- src/services/manager.ts
+++ src/services/manager.ts
@@ -3,8 +3,9 @@ import OrderStatus from '../enums/order-status-enum';
 
 export const updateOrderStatus = (orderId: string, status: number, message?: string) => {
 	switch (status) {
-		case OrderStatus.APPROVED: return Order.findByIdAndUpdate(orderId, { status, message }, { new: true })
-		case OrderStatus.CANCELED: return Order.findByIdAndUpdate(orderId, { status, message }, { new: true })
+		case OrderStatus.APPROVED:
+		case OrderStatus.CANCELED:
+		case OrderStatus.RETURN_DENIED: return Order.findByIdAndUpdate(orderId, { status, message }, { new: true })
 		default: return Order.findByIdAndUpdate(orderId, { status }, { new: true })
 	}
 }
