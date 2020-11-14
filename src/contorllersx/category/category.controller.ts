1335e43582bb3548ede9e3d10bcdf1fe735f9aaa
Muhammet İpek <muhammetipek57@hotmail.com>
2020-11-14 19:40:06 +0300

    category SoC

--- src/contorllersx/category/category.controller.ts
+++ src/contorllersx/category/category.controller.ts
@@ -1,10 +1,22 @@
 import { Router } from 'express'
-import { validateDeleteSubCategory, validatePostCategory, validatePostSubCategory, validateUpdateCategory, validateUpdateSubCategory } from '../../validators/admin-validator'
-import { deleteCategoryFromDatabase, deleteSubCategoryFromDatabase, getSeoUrl, isCategorySlugExists, isSubCategorySlugExists, saveCategoryToDatabase, saveSubCategoryToDatabase, updateCategory, updateSubCategory } from '../../services/admin'
-
 import {
-    getCategories
-} from '../../services/unauthorized'
+    deleteSubCategorySchema,
+    categorySchema,
+    saveSubCategorySchema,
+    updateSubCategorySchema
+} from './category.validator'
+import {
+    getCategories,
+    deleteCategoryFromDatabase,
+    deleteSubCategoryFromDatabase,
+    getSeoUrl,
+    isCategorySlugExists,
+    isSubCategorySlugExists,
+    saveCategoryToDatabase,
+    saveSubCategoryToDatabase,
+    updateCategory,
+    updateSubCategory
+} from './category.service'
 
 const router = Router()
 
@@ -15,7 +27,7 @@ router.get('/', async (_, res) => {
 })
 
 router.post('/category', async (req, res) => {
-    await validatePostCategory(req.body)
+    await categorySchema.validateAsync(req.body)
     const slug = getSeoUrl(req.body.name)
     await isCategorySlugExists(slug)
     const category = await saveCategoryToDatabase({ ...req.body, slug })
@@ -30,7 +42,7 @@ router.delete('/:_id', async (req, res) => {
 })
 
 router.put('/:_id', async (req, res) => {
-    await validateUpdateCategory(req.body)
+    await categorySchema.validateAsync(req.body)
     const slug = getSeoUrl(req.body.name)
     await isCategorySlugExists(slug, req.params._id)
     const category = await updateCategory(req.params._id, { ...req.body, slug })
@@ -39,7 +51,7 @@ router.put('/:_id', async (req, res) => {
 })
 
 router.post('/sub-category', async (req, res) => {
-    await validatePostSubCategory(req.body)
+    await saveSubCategorySchema.validateAsync(req.body)
     const slug = getSeoUrl(req.body.name)
     await isSubCategorySlugExists(req.body, slug)
     const category = await saveSubCategoryToDatabase({ ...req.body, slug })
@@ -48,14 +60,14 @@ router.post('/sub-category', async (req, res) => {
 })
 
 router.delete('/sub-category/:parentCategoryId/:_id', async (req, res) => {
-    await validateDeleteSubCategory(req.params)
+    await deleteSubCategorySchema.validateAsync(req.params)
     const category = await deleteSubCategoryFromDatabase(req.query)
 
     res.json(category)
 })
 
 router.put('/sub-category', async (req, res) => {
-    await validateUpdateSubCategory(req.body)
+    await updateSubCategorySchema.validateAsync(req.body)
     const slug = getSeoUrl(req.body.name)
     await isSubCategorySlugExists(req.body, slug)
     const category = await updateSubCategory(req.body, slug)
---
+++ src/contorllersx/category/category.service.ts
@@ -0,0 +1,186 @@
+import HttpStatusCodes from 'http-status-codes'
+
+import {
+	Category,
+	ProductDocument,
+	CategoryDocument,
+    ProductType,
+} from '../../models'
+import Brand from '../../models/Brand'
+import ServerError from '../../errors/ServerError'
+import ErrorMessages from '../../errors/ErrorMessages'
+
+export const getSeoUrl = (name: string) => {
+	if (!name) {
+		return name
+	}
+
+	return name.toString() // Convert to string
+		.normalize('NFD') // Change diacritics
+		.replace(/[\u0300-\u036f]/g, '') // Remove illegal characters
+		.replace(/\s+/g, '-') // Change whitespace to dashes
+		.split('ı')
+		.join('i')
+		.toLowerCase() // Change to lowercase
+		.replace(/&/g, '-and-') // Replace ampersand
+		.replace(/[^a-z0-9\-]/g, '') // Remove anything that is not a letter, number or dash
+		.replace(/-+/g, '-') // Remove duplicate dashes
+		.replace(/^-*/, '') // Remove starting dashes
+		.replace(/-*$/, '') // Remove trailing dashes
+}
+
+export const getCategories = () => (
+	Category.aggregate([
+		{
+			$unwind: {
+				path: '$subCategories',
+				preserveNullAndEmptyArrays: true
+			}
+		},
+		{
+			$addFields: {
+				subCategories: { $ifNull: ['$subCategories', { types: [] }] }
+			}
+		},
+		{
+			$lookup: {
+				from: ProductType.collection.name,
+				let: { types: '$subCategories.types' },
+				pipeline: [
+					{
+						$match: {
+							$expr: {
+								$in: ['$_id', '$$types']
+							}
+						}
+					}
+				],
+				as: 'subCategories.types'
+			}
+		},
+		{
+			$group: {
+				_id: '$_id',
+				imagePath: { $first: '$imagePath' },
+				name: { $first: '$name' },
+				slug: { $first: '$slug' },
+				brands: { $first: '$brands' },
+				subCategories: { $push: '$subCategories' }
+			}
+		},
+		{
+			$project: {
+				_id: 1,
+				imagePath: 1,
+				name: 1,
+				slug: 1,
+				brands: 1,
+				subCategories: {
+					$filter: {
+						input: '$subCategories',
+						as: 'subCategory',
+						cond: { $gt: ['$$subCategory._id', null] }
+					}
+				}
+			}
+		},
+		{
+			$sort: {
+				imagePath: 1
+			}
+		}
+	])
+)
+
+
+export const saveCategoryToDatabase = (categoryContext: CategoryDocument) => {
+	return new Category(categoryContext).save()
+}
+
+export const saveSubCategoryToDatabase = (body: any) => {
+	return Category.findByIdAndUpdate(body.parentCategoryId, {
+		$push: {
+			subCategories: body
+		}
+	}, { new: true })
+}
+
+export const deleteSubCategoryFromDatabase = (body: any) => {
+	return Category.findByIdAndUpdate(body.parentCategoryId, {
+		$pull: {
+			subCategories: {
+				_id: body._id
+			}
+		}
+	}, { new: true })
+}
+
+export const deleteCategoryFromDatabase = async (categoryId: string) => {
+	const category = await Category.findById(categoryId)
+
+	if (!category) {
+		throw new ServerError(ErrorMessages.CATEGORY_IS_NOT_EXISTS, HttpStatusCodes.BAD_REQUEST, ErrorMessages.CATEGORY_IS_NOT_EXISTS, false)
+	}
+
+	return await category.remove()
+}
+
+export const updateCategory = (categoryId: string, categoryContext: CategoryDocument) => {
+	return Category.findByIdAndUpdate(categoryId, categoryContext)
+}
+
+export const isSubCategorySlugExists = async (body: any, slug: string) => { // is category exists ? isSubCategoryExists ? test. // TODO
+	const parentCategory = await Category.findById(body.parentCategoryId)
+	const subCategory = parentCategory.subCategories.find((subCategory) => subCategory.slug === slug)
+
+	if (subCategory && body.subCategoryId !== subCategory._id.toString()) {
+		throw new ServerError(ErrorMessages.ANOTHER_SUB_CATEGORY_WITH_THE_SAME_NAME, HttpStatusCodes.BAD_REQUEST, ErrorMessages.ANOTHER_SUB_CATEGORY_WITH_THE_SAME_NAME, false)
+	}
+
+	return slug
+}
+
+export const updateSubCategory = async (body: any, slug: string) => { // is category exists ? isSubCategoryExists ? test. // TODO
+	const parentCategory = await Category.findById(body.parentCategoryId)
+	const subCategory = parentCategory.subCategories.find((subCategory) => subCategory._id.toString() === body.subCategoryId)
+	subCategory.name = body.name
+	subCategory.types = body.types
+	subCategory.slug = slug
+
+	return parentCategory.save()
+}
+
+export const isCategorySlugExists = async (slug: string, updateId?: string) => {
+	const category = await Category.findOne({ slug })
+	if (category && updateId !== category._id.toString()) {
+		throw new ServerError(ErrorMessages.ANOTHER_CATEGORY_WITH_THE_SAME_NAME, HttpStatusCodes.BAD_REQUEST, ErrorMessages.ANOTHER_CATEGORY_WITH_THE_SAME_NAME, false)
+	}
+
+	return slug
+}
+
+export const updateCategoryOfProduct = async (product: ProductDocument) => {
+	const category = await Category.findById(product.categoryId.toString())
+
+	const productCategoryBrand = category.brands.find((brand) => brand.name === product.brand)
+	const productSubCategory = category.subCategories.find((subCategory) => subCategory._id.toString() === product.subCategoryId.toString())
+	const productSubCategoryBrand = productSubCategory.brands.find((brand) => brand.name === product.brand)
+
+	if (productCategoryBrand) {
+		// eslint-disable-next-line no-param-reassign
+		category.brands[category.brands.indexOf(productCategoryBrand)].productQuantity++
+
+		if (productSubCategoryBrand) {
+			productSubCategory.brands[productSubCategory.brands.indexOf(productSubCategoryBrand)].productQuantity++
+		} else {
+			productSubCategory.brands.push(new Brand({ name: product.brand, productQuantity: 1 }))
+		}
+	} else {
+		category.brands.push(new Brand({ name: product.brand, productQuantity: 1 }))
+		productSubCategory.brands.push(new Brand({ name: product.brand, productQuantity: 1 }))
+	}
+
+	await category.save()
+
+	return product
+}
